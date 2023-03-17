3f2b86966806b03bcde89fd34fe21a8542c96159
MASAMIAOI <ruiyan_shen@163.com>
2023-03-17 19:28:30 +0800

    fix - [example] - 变更 pdf 导出方法

--- exampleDemo/src/main/java/com/masamiaoi/exampledemo/util/PdfUtils.java
+++ exampleDemo/src/main/java/com/masamiaoi/exampledemo/util/PdfUtils.java
@@ -1,9 +1,14 @@
 package com.masamiaoi.exampledemo.util;
 
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +16,7 @@ import java.io.*;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -22,6 +28,95 @@ import java.util.Map;
 @Slf4j
 public class PdfUtils {
 
+    /**
+     * 根据模板创建生成pdf
+     *
+     * @return
+     */
+    public static void createPdfByTemplate() {
+        // 生成的新文件路径
+        String pdfPath = "F:\\ms\\ms\\javaproject\\pdfTest\\测试pdf.pdf";
+        // 图片位置
+        String imgPath = new ClassPathResource("img/1.jpg").getPath();
+        // dpf模板位置
+        String templatePath = new ClassPathResource("pdf/workOrder.pdf").getPath();
+        PdfStamper stamper = null;
+        PdfReader reader = null;
+        FileOutputStream fos = null;
+        ByteArrayOutputStream bos = null;
+        try {
+            // 读取pdf模板
+            reader = new PdfReader(templatePath);
+            //获取字节数组输出流
+            bos = new ByteArrayOutputStream();
+            //操作pdf文件
+            stamper = new PdfStamper(reader, bos);
+            //获取pdf模板中的属性
+            AcroFields form = stamper.getAcroFields();
+            //设置中文字体
+//            BaseFont bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+//            form.addSubstitutionFont(bf);
+            //处理模板文字部分
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("AssetName", "99999");
+            map.put("AssetId", "aaaa有限公司");
+            map.put("AssetTypeName", "安徽省马鞍山市花山区民族大道1099号大学城美食城A333092湖北省武汉社想下去金融港青年公寓");
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                form.setFieldProperty(entry.getKey(), "textsize", 8f, null);
+                form.setField(entry.getKey(), entry.getValue());
+            }
+            //处理模板图片
+            if (StringUtils.isNotBlank(imgPath) && null != form.getFieldPositions("img")) {
+                int pageNo = form.getFieldPositions("img").get(0).page;
+                Rectangle signRect = form.getFieldPositions("img").get(0).position;
+                float x = signRect.getLeft();
+                float y = signRect.getBottom();
+                //根据路径读取图片
+                Image image = Image.getInstance(imgPath);
+                //获取图片页面
+                PdfContentByte under = stamper.getOverContent(pageNo);
+                //图片大小自适应
+                image.scaleToFit(signRect.getWidth(), signRect.getHeight());
+                //添加图片
+                image.setAbsolutePosition(x, y);
+                under.addImage(image);
+            }
+            // 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
+            stamper.setFormFlattening(true);
+            //生成pdf路径
+            fos = new FileOutputStream(pdfPath);
+            fos.write(bos.toByteArray());
+            fos.flush();
+            System.out.println("pdf模板生成了");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != stamper) {
+                try {
+                    stamper.close();
+                } catch (IOException | DocumentException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (null != reader) {
+                reader.close();
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 
     /**
      * pdf 文档数据填充
